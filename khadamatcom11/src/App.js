4fb9db6028abce9492f54fa9b3bf0373e167bf81
majd <majd@gmail.com>
2024-10-01 20:57:30 +0300

    Updating on Service Component by m.mnaser

--- khadamatcom11/src/App.js
+++ khadamatcom11/src/App.js
@@ -3,24 +3,21 @@ import './App.css';
 import WelcomeBoard from './components/WelcomeBoard';
 import Services from './components/Services';
 import NavBar from './components/NavBar';
-import "./data.js";
-import { servicesCards } from './data.js';
+import { servicesCards } from './data.js'; // تأكد من عدم الحاجة لاستيراد "./data.js" مجددًا
 
 function App() {
   return (
     <div className="App">
       <div className='component'>  
         <NavBar/>
-     <WelcomeBoard />
-     <p className='p-under-welcomeboard'>Popular services</p>
-     <div className='Services'>
-     <Services {...servicesCards[0]}/>
-     <Services {...servicesCards[1]}/>
-     <Services {...servicesCards[2]}/>
-     </div>
-    
-     </div>
-     
+        <WelcomeBoard />
+        <p className='p-under-welcomeboard'>Popular services</p>
+        <div className='Services'>
+          {servicesCards.map((service, index) => (
+            <Services key={index} {...service} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
