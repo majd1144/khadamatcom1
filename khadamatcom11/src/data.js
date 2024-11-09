c2c511217632a9b1404831e8777847e41956fa3c
majd <majd@gmail.com>
2024-11-09 19:18:52 +0300

    styling css of services by manaseer

--- khadamatcom11/src/data.js
+++ khadamatcom11/src/data.js
@@ -1,31 +1,51 @@
-import pic1 from "./asset/teacher.jfif";
-import pic2 from "./asset/plumber.jfif";
-import pic3 from "./asset/Physical Therapist.jfif";
-
+import pic1 from "./asset/s1.jpeg";
+import pic2 from "./asset/s2.jpeg";
+import pic3 from "./asset/s3.jpeg";
+import pic4 from "./asset/s4.jpeg";
+import pic5 from "./asset/s5.jpeg";
+import pic6 from "./asset/s6.jpeg";
+import pic7 from "./asset/s7.jpeg";
+import pic8 from "./asset/s8.jpeg";
+import pic9 from "./asset/s9.jpeg";
+import pic10 from "./asset/s10.jpeg";
 
  export const servicesCards=[
     {
         image: pic1,
-        title: 'Teacher',       
+        title: 'Part time worker',       
       },
     {
-        image: pic1,
-        title: 'Teacher',       
-      },
-      {
         image: pic2,
-        title: 'plumber',
+        title: 'Painter',       
       },
       {
-        image:pic3,
-        title: 'Physical Therapist',
+        image: pic3,
+        title: 'Photographer',
       },
       {
-        image:pic3,
-        title: 'Physical Therapist',
+        image:pic4,
+        title: 'Carpenter',
       },
       {
-        image: pic1,
-        title: 'Teacher',       
+        image:pic5,
+        title: 'Graphic Designer',
+      },
+      {
+        image: pic6,
+        title: 'Learning',       
+      },
+      {
+        image:pic7,
+        title: 'Blacksmith',
+      },
+      {
+        image:pic8,
+        title: 'Wall painter',
+      },{
+        image:pic9,
+        title: 'Babysitter',
+      },{
+        image:pic10,
+        title: 'House maid',
       }
 ];
