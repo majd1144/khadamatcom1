e56dbb8c85eb479058290d7daf20383773b470bb
majdsaadat <majdsaadat9@gmail.com>
2024-11-17 22:46:31 +0300

    ll

--- khadamatcom11/src/components/ServicesCards.jsx
+++ khadamatcom11/src/components/ServicesCards.jsx
@@ -1,7 +1,5 @@
 import Services from '../components/Services';
 import React from 'react';
-//import { servicesCards } from '../data.js';
-//import "C:\Users\lenovo\Desktop\khadamatcom1\khadamatcom11\src\components\Navbar\Navbar.css";
 export default function ServicesCards({services}){
     return(
         <div id='servicescard' className='Services'>
