cffcc2c925a85eab8aa636a4d19e27c42c681bb2
majdsaadat <majdsaadat9@gmail.com>
2025-02-03 00:55:02 +0300

    change spelling of footer

--- khadamatcom11/src/App.js
+++ khadamatcom11/src/App.js
@@ -6,7 +6,7 @@ import Join from "./components/Join.jsx";
 import Signin from "./components/Signin.jsx";
 import ServicesIn from './components/ServicesIn.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
-import Footerr from './components/footerr.jsx'
+import Footerr from './components/Footerr.jsx'
 
 export default function App() {
     const [theme, setTheme] = useState('light');
